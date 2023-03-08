5f008ec255a2f5844922c1d52ccfc395fc8a9acd
bl4d3tv <10796919+bl4d3tv@users.noreply.github.com>
2023-03-08 15:56:32 -0300

    chore: config for GH plus update deps

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,15 +16,17 @@ const config = {
   url: "https://docs.notienenombre.net.ar",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "NoTieneNombre", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
+
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
